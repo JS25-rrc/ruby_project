311abef45940f59817c96be31f74a3a2ce130488
JS25-rrc <jsingh204@rrc.ca>
2026-03-24 17:50:22 -0500

    Add model associations for products and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,15 @@
 class Category < ApplicationRecord
-    validates :name, presence: true
-    validates :slug, presence: true, uniqueness: true
+  has_many :product_categories
+  has_many :products, through: :product_categories
 
-    def self.ransackable_attributes(auth_object = nil)
-        ["id", "name", "description", "slug", "created_at", "updated_at"]
-    end
+  validates :name, presence: true
+  validates :slug, presence: true, uniqueness: true
 
-    def self.ransackable_associations(auth_object = nil)
-        []
-    end
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "name", "description", "slug", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["products"]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,17 @@
 class Product < ApplicationRecord
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
-    validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 
-    def self.ransackable_attributes(auth_object = nil)
-        ["created_at", "description", "id", "name", "on_sale", "price", "sale_price", "stock_quantity", "updated_at"]
-    end
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-    def self.ransackable_associations(auth_object = nil)
-        []
-    end
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "name", "on_sale", "price", "sale_price", "stock_quantity", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["categories"]
+  end
 end
