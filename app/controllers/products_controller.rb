6d4d4ef221684c80d42341c10d08fae4d39e7ec5
JS25-rrc <jsingh204@rrc.ca>
2026-03-27 19:01:29 -0500

    Add pagination to products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
     @products = @products.on_sale if params[:filter] == "on_sale"
     @products = @products.new_arrivals if params[:filter] == "new"
     @products = @products.recently_updated if params[:filter] == "recently_updated"
+    @products = @products.page(params[:page]).per(12)
     session[:last_visited] = "products"
   end
 
