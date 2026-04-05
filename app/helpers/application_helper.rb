75bb93c6247c0b1bbfa8b8d1b84292644d8c5d41
JS25-rrc <jsingh204@rrc.ca>
2026-04-04 22:03:31 -0500

    Add location-based breadcrumbs up to three levels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def breadcrumbs(*crumbs)
+    content_tag :nav, "aria-label": "breadcrumb" do
+      content_tag :ol, class: "breadcrumb" do
+        crumbs.map.with_index do |crumb, i|
+          if i == crumbs.length - 1
+            content_tag :li, crumb[:name], class: "breadcrumb-item active"
+          else
+            content_tag :li, class: "breadcrumb-item" do
+              link_to crumb[:name], crumb[:path]
+            end
+          end
+        end.join.html_safe
+      end
+    end
+  end
 end
